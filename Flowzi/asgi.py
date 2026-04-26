0e0066506d76c8603e9db298b95a6dc03ec5ed8b
Gopika Santhosh <ggopika455@gmail.com>
2026-04-26 10:56:41 +0530

    fix asgi init order - setup Django before routing imports

--- Flowzi/asgi.py
+++ Flowzi/asgi.py
@@ -1,14 +1,14 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Flowzi.settings')
+
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
 from Flowzi.routing import websocket_urlpatterns
-
-
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
